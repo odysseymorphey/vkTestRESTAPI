236b3c7ff75cf935f4a16404c881dd471dc985e3
fredacha <fredacha@student.21-school.ru>
2024-03-18 19:42:43 +0300

    fix mistake

--- internal/cases/filmary_service.go
+++ internal/cases/filmary_service.go
@@ -62,3 +62,12 @@ func (s *FilmaryService) CreateMovie(ctx context.Context, movie dto.Movie) error
 
 	return err
 }
+
+func (s *FilmaryService) DeleteMovie(ctx context.Context, movie dto.Movie) error {
+	err := s.storage.DeleteMovie(ctx, movie)
+	if err != nil {
+		s.log.Error(err)
+	}
+
+	return err
+}
