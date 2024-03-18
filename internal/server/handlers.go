f59d5113e0367b30a3e206ab1a7d46931fe56da8
fredacha <fredacha@student.21-school.ru>
2024-03-18 08:32:26 +0300

    minor changes

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -7,6 +7,10 @@ import (
 	"strconv"
 )
 
+func (s *Server) mock(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func (s *Server) createActor(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		ctx := r.Context()
@@ -74,5 +78,13 @@ func (s *Server) deleteActor(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		s.log.Error("Method not allowed: ", r.Method)
+	}
+}
+
+func (s *Server) createMovie(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	}
 }
